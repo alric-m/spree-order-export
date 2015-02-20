9c88e9abc41977461545bd27895013ab266ab56d
alric-m <matthias.alric@epitech.eu>
2015-02-20 16:27:57 +0100

    commentting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-Spree::Core::Engine.routes.draw do
-  # Add your extension routes here
-  namespace :admin do
-    resources :reports do
-      get 'order_export', :on => :collection
-      get 'sales_report', :on => :collection
-    end
-  end
-end
+# Spree::Core::Engine.routes.draw do
+#   # Add your extension routes here
+#   namespace :admin do
+#     resources :reports do
+#       get 'order_export', :on => :collection
+#       get 'sales_report', :on => :collection
+#     end
+#   end
+# end
 
